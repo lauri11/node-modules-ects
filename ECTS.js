0c8d8cffa04d8d5e4b72d254a5b0ac525ab2f766
Lena <elena.korzhuk@gmail.com>
2023-11-22 09:37:42 +0200

    Initial commit

---
+++ ECTS.js
@@ -0,0 +1,21 @@
+export default class ECTS {
+    constructor(score) {
+        this.score = score;
+    }
+
+    ectsFromScore() {
+        if (this.score >= 90) {
+            return 'A';
+        } else if (this.score >= 80) {
+            return 'B';
+        } else if (this.score >= 70) {
+            return 'C';
+        } else if (this.score >= 60) {
+            return 'D';
+        } else if (this.score >= 50) {
+            return 'E';
+        } else {
+            return 'F';
+        }
+    }
+}
