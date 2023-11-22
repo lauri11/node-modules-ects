80df1b1b74327b255cd7707c498498846e179300
Lena <elena.korzhuk@gmail.com>
2023-11-22 10:09:31 +0200

    Fixed grade converting logic

--- index.js
+++ index.js
@@ -6,15 +6,20 @@ class ECTS {
     ectsFromScore() {
         if (this.score >= 90) {
             return 'A';
-        } else if (this.score >= 80) {
+        }
+        if (this.score >= 82 && this.score <= 89) {
             return 'B';
-        } else if (this.score >= 70) {
+        }
+        if (this.score >= 74 && this.score <=81) {
             return 'C';
-        } else if (this.score >= 60) {
+        }
+        if (this.score >= 65 && this.score <=73) {
             return 'D';
-        } else if (this.score >= 50) {
+        }
+        if (this.score >= 60 && this.score <= 64) {
             return 'E';
-        } else {
+        }
+        if(this.score >= 0 && this.score <= 59) {
             return 'F';
         }
     }
